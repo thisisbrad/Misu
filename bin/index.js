1f11eb75252c00540ac46b5f7a89099707adefa9
Brad Beltowski <bradbeltowski@gmail.com>
2018-01-20 02:15:45 -0500

    Basic Node server running a service.

---
+++ bin/index.js
@@ -0,0 +1,13 @@
+const service = require('../server/service');
+const http = require('http');
+
+const server = http.createServer(service);
+server.listen(3000);
+
+server.on('listening', () => {
+  console.log(
+    `Misu is listening on port:${server.address().port} in ${service.get(
+      'env'
+    )} mode.`
+  );
+});
---
+++ server/service.js
@@ -0,0 +1,5 @@
+const express = require('express');
+
+const service = express();
+
+module.exports = service;
